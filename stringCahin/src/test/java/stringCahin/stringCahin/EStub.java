1bce74971b477a64c9decd5a3fd7884d4d2a18e6
Osamaas1 <101986542+Osamaas1@users.noreply.github.com>
2025-11-17 12:57:14 +0200

    trgil_1

---
+++ stringCahin/src/test/java/stringCahin/stringCahin/EStub.java
@@ -0,0 +1,9 @@
+package stringCahin.stringCahin;
+public class EStub extends E {
+    public  EStub() {super(null);}
+	
+    @Override
+    public String process(String prefix) {
+        return prefix + "X"; // הוספה מדומה
+    }
+}
